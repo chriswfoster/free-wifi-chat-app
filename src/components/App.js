945a1887842c0792967ef8f5df9b36c525a45d59
ChriswFoster <chriswfoster@gmail.com>
2018-04-23 12:04:04 -0500

    Building hamburger menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import ChatWindow from './ChatWindow/ChatWindow'
-
+import HamburgerMenu from './HamburgerMenu/HamburgerMenu'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <HamburgerMenu />
       <ChatWindow />
       </div>
     );
--- src/components/HamburgerMenu/HamburgerMenu.js
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
-
+import { stack as Menu } from "react-burger-menu"
 class HamburgerMenu extends Component {
 
 
 render() {
 return(
-<div>
-    
+<div className="navbarbackground">
+<Menu customBurgerIcon={<img src={require("./menu.png")} alt="Button icon"/>}>
+  <div> Menu / Info: </div>
+  <p>
+    <button onClick={()=> alert("Open your device/phone's wifi settings and disconnect from this wifi. You can usually tell your device to 'forget this network' as well.")}> How do I disconnect from this?</button>
+  </p>
+  <p>
+  Another item</p>
+  <p>
+    Another item2
+  </p>
+  <p>
+    Another item3
+  </p>
+</Menu>
 </div>
 )}
 }
